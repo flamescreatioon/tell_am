adde501f776b624303a326016f80588edd97fd87
Ugochukwu  Nelson Achor <92747656+flamescreatioon@users.noreply.github.com>
2025-04-17 14:34:29 +0100

    First Commit

---
+++ android/app/src/main/kotlin/com/example/tell_am/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tell_am
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  LoginScreenState createState() => LoginScreenState();
+}
+
+class LoginScreenState extends State<LoginScreen>{
+
+  @override
+  Widget build(BuildContext context){
+    return const Scaffold(
+      body: SafeArea(
+        child: Center (
+          child: Padding(
+            padding: EdgeInsets.all(24.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+
+                Center(
+                  child: Icon(
+                    Icons.lock,
+                    size: 100,
+                    color: Colors.blue,
+                  ),
+                ),
+
+                const SizedBox(height: 24.0),
+
+                Text(
+                  'Welcome Back',
+                  style: TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue,
+                  
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+
+              ],
+            )
+          )
+        )
+      ) 
+    );
+  }
+}
