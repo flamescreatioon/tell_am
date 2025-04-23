5023c0b25b254094101c914a249385009eb54841
Ugochukwu  Nelson Achor <92747656+flamescreatioon@users.noreply.github.com>
2025-04-23 15:38:55 +0100

    Bottom navbar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 
 import 'package:flutter/widgets.dart';
+import 'package:tell_am/components/bottom_navbar.dart';
 
 class HomeScreen extends StatefulWidget{
   const HomeScreen({Key?key}):super(key:key);
@@ -427,50 +428,7 @@ Widget build(BuildContext context){
         ),
       ),
       ),
-       bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        selectedItemColor: Colors.orange,
-        unselectedItemColor: Colors.grey,
-        currentIndex: 0,
-        onTap: (index) {
-          switch (index) {
-            case 0:
-              // Current page - Home
-              break;
-            case 1:
-              Navigator.pushNamed(context, '/menu');
-              break;
-            case 2:
-              Navigator.pushNamed(context, '/orders-history');
-              break;
-            case 3:
-              Navigator.pushNamed(context, '/profile');
-              break;
-          }
-        },
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home_outlined),
-            activeIcon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.restaurant_menu_outlined),
-            activeIcon: Icon(Icons.restaurant_menu),
-            label: 'Menu',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.receipt_long_outlined),
-            activeIcon: Icon(Icons.receipt_long),
-            label: 'Orders',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person_outline),
-            activeIcon: Icon(Icons.person),
-            label: 'Profile',
-          ),
-        ],
-      ),
+      bottomNavigationBar: const BottomNavbar(),
     );
   }
 
